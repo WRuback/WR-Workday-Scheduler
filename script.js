1d69797d76765fd0319140a4094a038947cabb69
William Ruback <rubackw@gmail.com>
2022-01-05 18:22:47 -0500

    Added row builder, and loop to create all rows.

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 var today = $("#currentDay");
-var schedule = $(".container");
+var schedule = $("#scheduler");
+var plans = "";
 
-today.text(moment().format("MMMM Do, YYYY"));
+function init(){
+    today.text(moment().format("MMMM Do, YYYY"));
+    if(localStorage.getItem("plans") === null){
+        localStorage.setItem("plans", JSON.stringify([
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]));
+    }
+    plans = JSON.parse(localStorage.getItem("plans"));
+    buildScheduler();
+}
+
+function buildRow(time, plan){
+    let scheduleRow = $("<div>");
+    scheduleRow.addClass("row");
+
+    let timeDisplay = $("<p>");
+    timeDisplay.addClass("col-2 hour text-right p-2");
+    timeDisplay.text(time.format("hA"));
+    scheduleRow.append(timeDisplay);
+
+    let planInput = $("<textarea>");
+    planInput.addClass("col-8 future");
+    planInput.text(plan);
+    scheduleRow.append(planInput);
+
+    let saveButton = $("<button>");
+    saveButton.addClass("col-2 btn saveBtn");
+    saveButton.append("<i>").addClass("fa fa-floppy-o fa-3x");
+    scheduleRow.append(saveButton);
+
+    schedule.append(scheduleRow);
+}
+function buildScheduler(){
+    let hour = moment("8AM", "hA");
+    for(let i =0;i<plans.length; i++){
+        hour.add(1, "h");
+        buildRow(hour, plans[i]);
+    }
+}
+
+
+init();
